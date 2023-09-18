649b0011f245bc71be9cb2e59a8bd1c25ddfa7fe
Sandykum <ssannthoshkumar@gmail.com>
2023-09-18 11:54:06 +0530

    test

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -355,7 +355,7 @@ export class ArticleComponent implements OnInit {
       category: item["category"],
       heading: item["heading"],
       tags: item["tags"],
-      description: item["description"],
+      description: item["description testing"],
       imageurl: item["thumbnail_url"],
       version: version,
       publishDate: publish_date
